edc7a6b17de9f68556281bdab95844653eca85f7
Kiki Priana <kikipriana@192.168.1.5>
2021-10-13 17:46:29 +0700

    push

--- application/views/masterdata/edit_item.php
+++ application/views/masterdata/edit_item.php
@@ -18,8 +18,20 @@
           <div class="card-body" style="overflow:auto;white-space: nowrap;">
             <div class="row">
               <div class="col-md-12 f-color1">
-                <h5><b>Data Umum</b></h5>
-                <hr>
+              <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+              </ul>
               </div>
               <div class="col-md-6">
                  <div class="form-group">

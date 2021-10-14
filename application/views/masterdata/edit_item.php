f8e8252c514abc95b86a3bf38e48d1ab0d10090e
Kiki Priana <kikipriana@192.168.1.5>
2021-10-14 16:12:29 +0700

    ad

--- application/views/masterdata/edit_item.php
+++ application/views/masterdata/edit_item.php
@@ -17,8 +17,8 @@
           <form method="post" action="<?php echo base_url(); ?>insert-item" id="frm-item">
           <div class="card-body" style="overflow:auto;white-space: nowrap;">  
             <div class="row">   
-              <div class="col-md-12" style="overflow:auto;white-space: nowrap;"> 
-                  <ul class="nav nav-tabs">
+              <div class="col-md-12"> 
+                  <ul class="nav nav-tabs"  style="overflow:auto;white-space: nowrap;">
                     <li class="nav-item">
                       <a class="nav-link active"  data-toggle="tab" href="#data-umum">Data Umum</a>
                     </li>

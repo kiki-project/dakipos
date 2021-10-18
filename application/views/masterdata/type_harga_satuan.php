4f7f8fc2f6650f2592281e0245ab6efd5b5fe533
Kiki Priana <kikipriana@192.168.1.6>
2021-10-18 21:09:12 +0700

    as

--- application/views/masterdata/type_harga_satuan.php
+++ application/views/masterdata/type_harga_satuan.php
@@ -1,16 +1,19 @@
 <table class="table table-bordered table-stripped">
-    <tr>
-        <th>No</th>
-        <th>Satuan</th>
-        <th>Jns Satuan</th>
-        <th>Jml Konversi</th>
-        <th>Barcode</th>
-        <th>Poin</th>
-        <th>Komisi</th>
-        <th>Harga Pokok</th>
-        <th>Proc (%)</th>
-        <th>Harga Jual</th>
-    </tr>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Satuan</th>
+            <th>Jns Satuan</th>
+            <th>Jml Konversi</th>
+            <th>Barcode</th>
+            <th>Poin</th>
+            <th>Komisi</th>
+            <th>Harga Pokok</th>
+            <th>Proc (%)</th>
+            <th>Harga Jual</th>
+        </tr>
+    </thead>
+    <tbody id="tbl-satuan_harga"></tbody>
 </table>
-<button class="btn btn-sm btn-primary">Tambah satuan</button>
+<button class="btn btn-sm btn-primary" onclick="add_satuan('<?php echo $id; ?> ')">Tambah satuan</button>
 <button class="btn btn-sm btn-danger">Hapus satuan</button>

f7be8f7db25d626d843c162a866e9f873a9497fe
Kiki Priana <kikipriana@192.168.1.6>
2021-10-19 08:05:00 +0700

    a

--- application/views/masterdata/type_harga_satuan.php
+++ application/views/masterdata/type_harga_satuan.php
@@ -45,8 +45,9 @@
                     data += '</td>';
                     data += '<td>'+row["jenis_satuan"]+'</td>';
                     data += '<td style="text-align:right;">'
+                    var id_knv = 'konv'+row['id']
                     if(row["jenis_satuan"] == "Konversi"){
-                    data += '<input type="text" class="form-control" value="'+row["konversi"]+'" id="'+'konv'+row["id"]+'" oninput="set_currency_value('+'''+'konv'+row["id"]+'''+', this.value)" style="text-align:right;">'
+                    data += '<input type="text" class="form-control" value="'+row["konversi"]+'" id="'+id_konv+'" oninput="set_currency_value('+id_konv+', this.value)" style="text-align:right;">'
                     }else{
                     data += '<input type="text" class="form-control" value="'+row["konversi"]+'" readonly style="text-align:right;">'
                     }

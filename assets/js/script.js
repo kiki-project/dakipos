9615519618a217e2f40d23e8d5988560cd2aa12d
Kiki Priana <kikipriana@192.168.1.5>
2021-11-08 01:13:26 +0700

    as

--- assets/js/script.js
+++ assets/js/script.js
@@ -745,7 +745,7 @@ function hitung_price_units(id, code,val){
   }else{
   var n_proc = proc;
   }
-  if(code == 1005 || code == 1004){
+  if(harga_jual != 0 && proc != 0){
   var laba = (parseInt(proc) * parseInt(harga_pokok)) / 100;
   var n_jual = (parseInt(harga_pokok) + parseInt(laba))
   }else{

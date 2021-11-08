ebc2be4ec47ca9fe6e78bda341dbb90888d83724
Kiki Priana <kikipriana@192.168.1.5>
2021-11-08 23:28:36 +0700

    ad

--- application/controllers/Masterdata/Items.php
+++ application/controllers/Masterdata/Items.php
@@ -368,6 +368,24 @@ class Items extends CI_Controller {
 		  		'harga_jual2' 	=> str_replace(',', '', $this->input->post('harga_jual2')),
 		  		'harga_jual3' 	=> str_replace(',', '', $this->input->post('harga_jual3')),
 			);
+		}elseif($type == 'jumlah'){
+			$data = array(
+		  		'satuan'		=> $this->input->post('satuan'),
+		  		'barcode'		=> $this->input->post('barcode'),
+		  		'konversi'		=> str_replace(',', '', $this->input->post('konversi')),
+		  		'poin'			=> str_replace(',', '', $this->input->post('poin')),
+		  		'komisi'		=> str_replace(',', '', $this->input->post('komisi')),
+		  		'harga_pokok'	=> str_replace(',', '', $this->input->post('harga_pokok')),
+		  		'jumlah'		=> str_replace(',', '', $this->input->post('jumlah')),
+		  		'jumlah2'		=> str_replace(',', '', $this->input->post('jumlah2')),
+		  		'jumlah3'		=> str_replace(',', '', $this->input->post('jumlah3')),
+		  		'proc'			=> str_replace(',', '', $this->input->post('proc')),
+		  		'proc2'			=> str_replace(',', '', $this->input->post('proc2')),
+		  		'proc3'			=> str_replace(',', '', $this->input->post('proc3')),
+		  		'harga_jual' 	=> str_replace(',', '', $this->input->post('harga_jual')),
+		  		'harga_jual2' 	=> str_replace(',', '', $this->input->post('harga_jual2')),
+		  		'harga_jual3' 	=> str_replace(',', '', $this->input->post('harga_jual3')),
+			);
 		}
 		$this->Mod_adm->update_price_units($data,$id);
 	}

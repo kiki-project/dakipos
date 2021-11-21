ad53d13c8f6e28255e0c55be97fa94a4310a84d1
Kiki Priana <kikipriana@192.168.1.5>
2021-11-21 15:05:10 +0700

    ad

--- application/views/purchase/edit_orders.php
+++ application/views/purchase/edit_orders.php
@@ -144,7 +144,7 @@
                   <div class="form-group col-md-6" id="frm-Kekurangan">
                     <label>Kekurangan:</label>
                     <div class="input-group ">
-                      <input type="text" name="kekurangan" style="text-align: right;" readonly="" id="kekurangan" class="form-control" value="<?php echo number_format($data['kekurangan'], 2); ?>">
+                      <input type="text" name="kredit" style="text-align: right;" readonly="" id="kekurangan" class="form-control" value="<?php echo number_format($data['kredit'], 2); ?>">
                       <div class="input-group-append">
                         <div class="btn btn-default" ><i class="fa fa-trash"></i></div>
                       </div>

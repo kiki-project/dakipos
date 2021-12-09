eb78e4e6988d6d13b1500c2992c69146f02af101
kiki-valbury <kiki.priana@id.valbury.com>
2021-12-09 19:12:45 +0700

    ad

--- application/views/purchase/table_order.php
+++ application/views/purchase/table_order.php
@@ -39,6 +39,7 @@
             url: base_url("json/get-purchase_item"),
             success: function (response) {            
                 result = JSON.parse(response);
+                console.log(result)
                 var data = "";
                 $.each(result, function (i, row) {
                     get_units_select_price_type('dimensi'+row['id'], row['satuan'])

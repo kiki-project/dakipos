4ff16cdaad32ffd933fe1ebbe6032130c4021f6f
kiki-valbury <kiki.priana@id.valbury.com>
2021-12-19 22:25:36 +0700

    ad

--- application/views/purchase/cetak_order.php
+++ application/views/purchase/cetak_order.php
@@ -1,2 +1,9 @@
 <div class="page-cetak">
+    <table>
+        <tr>
+            <td>
+                <img src="<?php echo base_url(); ?>assets/doc/logo/main_logo.png" alt="" style="width: 106px;height: 98px;">
+            </td>
+        </tr>
+    </table>
 </div>

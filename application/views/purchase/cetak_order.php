0d961744ac557b3e265b293d0cd72cbd4e76bf37
kiki-valbury <kiki.priana@id.valbury.com>
2021-12-19 22:42:46 +0700

    ad

--- application/views/purchase/cetak_order.php
+++ application/views/purchase/cetak_order.php
@@ -1,14 +1,18 @@
 <div class="page-cetak">
-    <table>
+    <table style="width:100%;">
         <tr>
-            <td>
+            <td colspan="3">
                 <img src="<?php echo base_url(); ?>assets/doc/logo/main_logo.png" alt="" style="width: 106px;height: 98px;">
             </td>
-            <td>
+            <td colspan="3">
                 <b>Pesanan Pembelian (PO)</b><br>
                 <b>DAKI STTM</b><br>
                 Jakarta
             </td>
+            <td style="text-align:right;">No Transaksi :</td>
+            <td style="text-align:left;"><?php echo $data['kode']; ?> :</td>
+            <td style="text-align:right;">Dept :</td>
+            <td style="text-align:left;"><?php echo $data['masuk_ke']; ?> :</td>
         </tr>
     </table>
 </div>

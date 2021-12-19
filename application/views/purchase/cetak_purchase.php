3aa797cb38f4d15ff35df57bd26a7f4a8597c58d
kiki-valbury <kiki.priana@id.valbury.com>
2021-12-20 02:13:35 +0700

    ad

--- application/views/purchase/cetak_purchase.php
+++ application/views/purchase/cetak_purchase.php
@@ -65,7 +65,7 @@
             <td style="text-align:right;" colspan="2"><?php echo number_format($data['sub_total_harga'],2); ?></td>
         </tr>
         <tr>
-            <td colspan="3" rowspan="4" style="vertical-align:top;"><i><?php echo $data['description']?></i></td>
+            <td colspan="3" rowspan="5" style="vertical-align:top;"><i><?php echo $data['description']?></i></td>
             <td style="text-align:right;">Potongan : </td>
             <td style="text-align:right;"><?php echo number_format($data['potongan'],2); ?></td>
             <td style="text-align:right;">Total Aktif : </td>

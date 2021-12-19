d304dbec18e44f10361075613c85d35407fb9cba
kiki-valbury <kiki.priana@id.valbury.com>
2021-12-19 22:55:31 +0700

    ad

--- application/views/purchase/cetak_order.php
+++ application/views/purchase/cetak_order.php
@@ -1,5 +1,5 @@
 <div class="page-cetak">
-    <table style="width:100%;">
+    <table style="width:100%;" border="1">
         <tr>
             <td colspan="3">
                 <img src="<?php echo base_url(); ?>assets/doc/logo/main_logo.png" alt="" style="width: 106px;height: 98px;">
@@ -17,12 +17,12 @@
         <tr>
             <td style="text-align:right;">Tanggal :</td>
             <td style="text-align:left;"><?php echo $data['tanggal']; ?></td>
-            <td style="text-align:right;">Supplier :</td>
-            <td style="text-align:left;"><?php echo $data['supplier']." ".$data['supplier_name']; ?></td>
-        </tr>
-        <tr>
             <td style="text-align:right;">User :</td>
             <td style="text-align:left;"><?php echo $data['user']; ?></td>
+        </tr>
+        <tr>
+            <td style="text-align:right;">Supplier :</td>
+            <td style="text-align:left;"><?php echo $data['supplier']." ".$data['supplier_name']; ?></td>
             <td style="text-align:right;">PPN :</td>
             <td style="text-align:left;"><?php echo $data['ppn']; ?></td>
         </tr>

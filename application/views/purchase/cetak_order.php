235080182f80cb5615a66aaf3ad3698acc1e6a26
kiki-valbury <kiki.priana@id.valbury.com>
2021-12-19 22:37:55 +0700

    ad

--- application/views/purchase/cetak_order.php
+++ application/views/purchase/cetak_order.php
@@ -5,8 +5,8 @@
                 <img src="<?php echo base_url(); ?>assets/doc/logo/main_logo.png" alt="" style="width: 106px;height: 98px;">
             </td>
             <td>
-                <h4>Pesanan Pembelian (PO)</h4><br>
-                <h4>DAKI STTM</h4><br>
+                <b>Pesanan Pembelian (PO)</b><br>
+                <b>DAKI STTM</b><br>
                 Jakarta
             </td>
         </tr>
